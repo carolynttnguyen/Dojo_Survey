e3b469701a950dee449ef4f6eb185876f5cce556
carongyn <carolynttnguyen@gmail.com>
2021-01-16 13:57:50 -0800

    Revisited assignment, altered method holding data to redirect. Also stored date in a request.session dictionary

--- survey_app/urls.py
+++ survey_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('survey', views.survey),
     path('result', views.result),
 ]
--- survey_app/views.py
+++ survey_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, redirect
 
+
 def index(request):
     return render(request, 'index.html')
 
+
+def survey(request):
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['answers'] = {
+        'name': request.POST['name'],
+        'location': request.POST['location'],
+        'language': request.POST['language'],
+        'comment': request.POST['comment']
+    }
+    return redirect('/result')
+
+
 def result(request):
-    if request.method == 'POST':
-        context = {
-            'name': request.POST['name'],
-            'location': request.POST['location'],
-            'language': request.POST['language'],
-            'comment': request.POST['comment'],
-        }
-        return render(request, 'result_page.html', context)
-    return render(request, 'result_page.html')
+    context = {
+        'answer': request.session['answers']
+    }
+    return render(request, 'result_page.html', context)
